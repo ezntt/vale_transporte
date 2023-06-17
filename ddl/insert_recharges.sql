b367cbfd9a83ece396ddf04087f68efc1ac51b41
PedroMatiucci <pmatiucci@gmail.com>
2023-06-17 16:55:31 -0300

    script recargas

---
+++ ddl/insert_recharges.sql
@@ -0,0 +1,134 @@
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('100', '400', '100', '40.00', '2023-06-16');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('101', '400', '105', '20.00', '2023-05-16');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('102', '400', '106', '15.00', '2023-04-11');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('103', '400', '100', '03.50', '2023-03-06');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('104', '400', '110', '22.75', '2023-03-26');
+
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('105', '401', '111', '22.75', '2021-12-25');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('106', '401', '111', '67.75', '2021-12-20');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('107', '401', '112', '12.75', '2021-10-05');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('108', '401', '100', '2.75', '2021-09-03');
+
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('109', '402', '115', '2.75', '2021-09-17');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('110', '402', '114', '12.50', '2021-08-03');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('111', '402', '103', '122.75', '2021-07-13');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('112', '402', '102', '10.00', '2021-03-27');
+
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('113', '403', '105', '255.50', '2022-03-11');
+
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('114', '404', '106', '15.50', '2022-08-31');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('115', '404', '114', '5.50', '2022-08-15');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('116', '404', '102', '50.20', '2022-04-05');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('117', '405', '106', '32.20', '2020-02-14');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('118', '406', '104', '9.20', '2022-11-25');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('119', '406', '104', '29.20', '2022-11-01');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('120', '407', '100', '129.20', '2022-09-30');
+
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('121', '408', '115', '47.20', '2022-09-02');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('122', '408', '104', '37.40', '2022-08-01');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('123', '408', '111', '19.70', '2022-07-13');
+
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('123', '409', '111', '19.70', '2023-04-13');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('124', '409', '101', '13.70', '2023-03-03');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('125', '409', '105', '46.60', '2023-02-12');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('126', '409', '106', '15.20', '2023-01-22');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('127', '409', '109', '25.00', '2022-12-25');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('128', '410', '102', '25.00', '2023-05-14');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('129', '410', '107', '33.30', '2023-03-20');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('130', '411', '101', '43.30', '2022-09-11');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('131', '411', '112', '22.10', '2022-07-26');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('132', '412', '101', '12.10', '2023-03-04');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('133', '412', '107', '48.20', '2023-02-18');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('134', '412', '110', '9.50', '2023-01-25');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('135', '413', '100', '19.20', '2022-12-04');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('136', '413', '100', '5.50', '2022-10-22');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('137', '414', '111', '12.80', '2023-06-07');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('138', '414', '115', '56.50', '2023-03-28');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('139', '415', '100', '6.50', '2023-01-06');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('140', '415', '109', '16.00', '2022-11-16');
+
+INSERT INTO Pedido_recarga (id_pedido, id_cartao, id_funcionario, valor, data)
+VALUES ('141', '415', '105', '28.90', '2023-09-02');
