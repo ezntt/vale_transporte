6aebed3fc5f94cb21c6a0fba04ab9c6c915cda71
PedroMatiucci <pmatiucci@gmail.com>
2023-06-17 19:11:48 -0300

    sql de inserir uso do cartao

---
+++ ddl/insert_card_uses.sql
@@ -0,0 +1,141 @@
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('100', '600', '400', '2023-06-17', '6:00');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('101', '600', '400', '2023-06-17', '18:00');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('102', '600', '400', '2023-04-17', '6:00');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('103', '600', '400', '2023-04-17', '18:00');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('104', '600', '400', '2023-04-14', '6:00');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('105', '600', '400', '2023-04-13', '18:00');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('106', '601', '403', '2022-09-13', '18:00');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('107', '601', '403', '2022-09-13', '6:00');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('108', '601', '409', '2022-09-13', '8:00');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('109', '601', '412', '2022-09-13', '12:00');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('110', '601', '403', '2022-09-13', '12:00');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('111', '601', '401', '2022-09-13', '8:00');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('112', '601', '409', '2022-09-13', '18:00');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('113', '601', '400', '2022-09-13', '13:30');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('114', '602', '415', '2022-08-12', '13:30');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('115', '602', '414', '2022-08-15', '18:30');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('116', '602', '415', '2022-08-14', '11:30');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('117', '603', '415', '2022-08-14', '11:30');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('118', '603', '412', '2022-07-10', '13:30');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('119', '605', '401', '2021-01-10', '19:30');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('120', '605', '402', '2021-01-10', '15:45');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('121', '606', '403', '2022-11-10', '14:50');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('122', '606', '404', '2021-01-13', '14:20');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('123', '606', '405', '2021-01-22', '16:30');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('124', '607', '409', '2023-03-22', '6:30');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('125', '607', '408', '2021-03-22', '17:30');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('126', '607', '411', '2021-01-26', '12:30');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('127', '608', '411', '2023-04-26', '9:30');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('128', '609', '413', '2023-02-19', '9:30');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('129', '609', '412', '2023-02-18', '16:45');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('130', '609', '400', '2022-09-02', '16:25');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('131', '609', '413', '2023-04-26', '9:30');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('132', '609', '406', '2023-04-26', '19:30');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('133', '610', '402', '2022-12-13', '19:30');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('134', '610', '402', '2022-11-15', '06:30');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('135', '611', '406', '2022-03-02', '8:30');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('136', '612', '403', '2023-01-02', '9:30');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('137', '612', '411', '2023-11-30', '8:45');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('138', '612', '414', '2023-09-29', '13:27');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('139', '612', '415', '2023-07-12', '12:14');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('140', '612', '414', '2023-10-13', '08:10');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('141', '612', '412', '2023-11-19', '8:15');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('142', '613', '404', '2022-12-04', '18:33');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('143', '613', '405', '2021-09-01', '11:30');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('144', '614', '401', '2023-04-12', '5:45');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('145', '614', '411', '2023-01-02', '17:23');
+
+INSERT INTO Uso_do_cartao (id_uso_cartao, id_catraca, id_cartao, data, hora)
+VALUES ('146', '614', '411', '2023-01-31', '10:10');
