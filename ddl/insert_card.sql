9fbcc5f0ae1b7a89e9685048157afa64153d9f81
PedroMatiucci <pmatiucci@gmail.com>
2023-06-17 13:36:29 -0300

    correcao tabela cartao

--- ddl/insert_card.sql
+++ ddl/insert_card.sql
@@ -1,47 +1,47 @@
 INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('id_usuario', '200', '32,00', '1', '2024-01-16', '2023-06-16');
+VALUES ('400', '200', '32,00', '1', '2024-01-16', '2023-06-16');
 
 INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('201', '63,00', '0', '2022-01-01', '2021-12-25');
+VALUES ('401','201', '63,00', '0', '2022-01-01', '2021-12-25');
 
 INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('202', '00,00', '0', '2021-11-14', '2021-09-17');
+VALUES ('402','202', '00,00', '0', '2021-11-14', '2021-09-17');
 
 INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('203', '12,50', '1', '2024-05-14', '2022-03-11');
+VALUES ('403','203', '12,50', '1', '2024-05-14', '2022-03-11');
 
 INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('204', '2,30', '1', '2023-11-22', '2022-08-31');
+VALUES ('404','204', '2,30', '1', '2023-11-22', '2022-08-31');
 
 INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('205', '17,20', '0', '2020-02-15', '2020-02-14');
+VALUES ('405','205', '17,20', '0', '2020-02-15', '2020-02-14');
 
 INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('206', '92,10', '1', '2025-05-11', '2022-11-25');
+VALUES ('406','206', '92,10', '1', '2025-05-11', '2022-11-25');
 
 INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('207', '36,00', '1', '2024-12-27', '2022-09-30');
+VALUES ('407','207', '36,00', '1', '2024-12-27', '2022-09-30');
 
 INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('208', '4,00', '1', '2025-03-01', '2022-09-02');
+VALUES ('408','208', '4,00', '1', '2025-03-01', '2022-09-02');
 
 INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('209', '16,03', '1', '2024-04-13', '2023-04-13');
+VALUES ('409','209', '16,03', '1', '2024-04-13', '2023-04-13');
 
 INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('210', '17,24', '1', '2024-05-14', '2023-05-14');
+VALUES ('410','210', '17,24', '1', '2024-05-14', '2023-05-14');
 
 INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('211', '37,42', '1', '2025-09-25', '2022-09-11');
+VALUES ('411','211', '37,42', '1', '2025-09-25', '2022-09-11');
 
 INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('212', '22,17', '1', '2025-01-23', '2023-03-04');
+VALUES ('412','212', '22,17', '1', '2025-01-23', '2023-03-04');
 
 INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('213', '17,22', '1', '2025-08-29', '2022-12-04');
+VALUES ('413','213', '17,22', '1', '2025-08-29', '2022-12-04');
 
 INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('214', '13,20', '1', '2023-07-28', '2023-06-07');
+VALUES ('414','214', '13,20', '1', '2023-07-28', '2023-06-07');
 
 INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('215', '19,35', '1', '2025-03-19', '2023-01-06');
+VALUES ('415','215', '19,35', '1', '2025-03-19', '2023-01-06');
