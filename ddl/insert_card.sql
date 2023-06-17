e0e14b66bdf10237f20421f9bddd5e902d8b998e
PedroMatiucci <pmatiucci@gmail.com>
2023-06-17 13:32:29 -0300

    Correcoes na data dos cartoes e criacao do users
    Criacao do scrip de ususarios e correcao da tabela de cartoes

--- ddl/insert_card.sql
+++ ddl/insert_card.sql
@@ -1,47 +1,47 @@
-INSERT INTO Cartao (id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('200', '32,00', '1', '26/01/2024', '16/06/2023');
+INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
+VALUES ('id_usuario', '200', '32,00', '1', '2024-01-16', '2023-06-16');
 
-INSERT INTO Cartao (id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('201', '63,00', '0', '01/01/2022', '25/12/2021');
+INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
+VALUES ('201', '63,00', '0', '2022-01-01', '2021-12-25');
 
-INSERT INTO Cartao (id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('202', '00,00', '0', '14/11/2021', '17/09/2021');
+INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
+VALUES ('202', '00,00', '0', '2021-11-14', '2021-09-17');
 
-INSERT INTO Cartao (id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('203', '12,50', '1', '14/05/2024', '11/03/2022');
+INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
+VALUES ('203', '12,50', '1', '2024-05-14', '2022-03-11');
 
-INSERT INTO Cartao (id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('204', '2,30', '1', '22/11/2023', '31/08/2022');
+INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
+VALUES ('204', '2,30', '1', '2023-11-22', '2022-08-31');
 
-INSERT INTO Cartao (id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('205', '17,20', '0', '15/02/2020', '14/02/2020');
+INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
+VALUES ('205', '17,20', '0', '2020-02-15', '2020-02-14');
 
-INSERT INTO Cartao (id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('206', '92,10', '1', '05/11/2025', '25/11/2022');
+INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
+VALUES ('206', '92,10', '1', '2025-05-11', '2022-11-25');
 
-INSERT INTO Cartao (id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('207', '36,00', '1', '27/12/2024', '30/09/2022');
+INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
+VALUES ('207', '36,00', '1', '2024-12-27', '2022-09-30');
 
-INSERT INTO Cartao (id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('208', '4,00', '1', '01/03/2025', '02/09/2022');
+INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
+VALUES ('208', '4,00', '1', '2025-03-01', '2022-09-02');
 
-INSERT INTO Cartao (id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('209', '16,03', '1', '13/04/2024', '13/04/2023');
+INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
+VALUES ('209', '16,03', '1', '2024-04-13', '2023-04-13');
 
-INSERT INTO Cartao (id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('210', '17,24', '1', '14/05/2024', '14/05/2023');
+INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
+VALUES ('210', '17,24', '1', '2024-05-14', '2023-05-14');
 
-INSERT INTO Cartao (id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('211', '37,42', '1', '25/09/2025', '11/09/2022');
+INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
+VALUES ('211', '37,42', '1', '2025-09-25', '2022-09-11');
 
-INSERT INTO Cartao (id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('212', '22,17', '1', '23/01/2025', '04/03/2023');
+INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
+VALUES ('212', '22,17', '1', '2025-01-23', '2023-03-04');
 
-INSERT INTO Cartao (id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('213', '17,22', '1', '29/08/2025', '04/12/2022');
+INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
+VALUES ('213', '17,22', '1', '2025-08-29', '2022-12-04');
 
-INSERT INTO Cartao (id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('214', '13,20', '1', '28/07/2023', '07/06/2023');
+INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
+VALUES ('214', '13,20', '1', '2023-07-28', '2023-06-07');
 
-INSERT INTO Cartao (id_cartao, saldo, status, Validade, ultima_recarga)
-VALUES ('215', '19,35', '1', '19/03/2025', '06/01/2023');
+INSERT INTO Cartao (id_usuario, id_cartao, saldo, status, Validade, ultima_recarga)
+VALUES ('215', '19,35', '1', '2025-03-19', '2023-01-06');
---
+++ ddl/insert_users.sql
@@ -0,0 +1,47 @@
+INSERT INTO Usuario (id_usuario, id_linha, nome, data_nascimento, cpf, email, telefone, bairro)
+VALUES ('400', '100', 'Gian Benites', '1997-01-30', '46003403020','Gianbenites@gmail.com', '88521-2095' ,'Centro');
+
+INSERT INTO Usuario (id_usuario, id_linha, nome, data_nascimento, cpf, email, telefone, bairro)
+VALUES ('401', '183', 'Adriano Albano', '1942-07-18', '42280728010','Adrianoalbano1@gmail.com', '81768-9819' ,'Campeche');
+
+INSERT INTO Usuario (id_usuario, id_linha, nome, data_nascimento, cpf, email, telefone, bairro)
+VALUES ('402', '134', 'Ivana Mia Cruz', '1974-05-21', '35449850042','ivanamia@gmail.com', '42368-1441' ,'Centro');
+
+INSERT INTO Usuario (id_usuario, id_linha, nome, data_nascimento, cpf, email, telefone, bairro)
+VALUES ('403', '200', 'Joel Ronaldo', '1962-04-06', '70345207041','joelronaldo@gmail.com', '82196-0729' ,'Daniela');
+
+INSERT INTO Usuario (id_usuario, id_linha, nome, data_nascimento, cpf, email, telefone, bairro)
+VALUES ('404', '300', 'Oliver Galhardo Neto', '1984-10-07', '92494999065','olivergalhardo@gmail.com', '79135-4787' ,'Itacorubi');
+
+INSERT INTO Usuario (id_usuario, id_linha, nome, data_nascimento, cpf, email, telefone, bairro)
+VALUES ('405', '461', 'Ricardo Paes', '1967-03-20', '72333579005','ricardopaes23@gmail.com', '52287-7102' ,'Centro');
+
+INSERT INTO Usuario (id_usuario, id_linha, nome, data_nascimento, cpf, email, telefone, bairro)
+VALUES ('406', '467', 'Alex ávila Neto', '1984-04-23', '28398818026','alexavila@gmail.com', '53716-1032' ,'Saco dos Limoes');
+
+INSERT INTO Usuario (id_usuario, id_linha, nome, data_nascimento, cpf, email, telefone, bairro)
+VALUES ('407', '604', 'Bóris Lira Filho', '1989-08-29', '85610830001','borislira12@gmail.com', '71717-8007' ,'Coqueiros');
+
+INSERT INTO Usuario (id_usuario, id_linha, nome, data_nascimento, cpf, email, telefone, bairro)
+VALUES ('408', '630', 'Bóris Sílvio Dominato', '1960-02-06', '93307089005','borissil@gmail.com', '10027-9357' ,'Sambaqui');
+
+INSERT INTO Usuario (id_usuario, id_linha, nome, data_nascimento, cpf, email, telefone, bairro)
+VALUES ('409', '762', 'Rafael Mário Gusmão', '1980-01-27', '83727808039','rafaelmario2@gmail.com', '62154-7198' ,'Trindade');
+
+INSERT INTO Usuario (id_usuario, id_linha, nome, data_nascimento, cpf, email, telefone, bairro)
+VALUES ('410', '763', 'Jeferson Leopoldo', '1966-10-18', '74336627070','jeferson12@gmail.com', '34318-7289' ,'Saco dos Limoes');
+
+INSERT INTO Usuario (id_usuario, id_linha, nome, data_nascimento, cpf, email, telefone, bairro)
+VALUES ('411', '767', 'Madalena Tatiane', '2004-04-30', '22916983031','madalenatati@gmail.com', '33963-6521' ,'Morro das pedras');
+
+INSERT INTO Usuario (id_usuario, id_linha, nome, data_nascimento, cpf, email, telefone, bairro)
+VALUES ('412', '772', 'Ângelo Camilo Neto', '2003-06-28', '89816313014','angelocamilo@gmail.com', '69663-4641' ,'Jardim atlantico');
+
+INSERT INTO Usuario (id_usuario, id_linha, nome, data_nascimento, cpf, email, telefone, bairro)
+VALUES ('413', '231', 'Sophie Priscila de Ferraz', '1962-09-29', '52184220027','Sophiepri@gmail.com', '18797-0863' ,'Bom abrigo');
+
+INSERT INTO Usuario (id_usuario, id_linha, nome, data_nascimento, cpf, email, telefone, bairro)
+VALUES ('414', '151', 'Ian Matos de Roque', '1986-10-15', '22287116087','IanMatos@gmail.com', '50634-5125' ,'Agronomica');
+
+INSERT INTO Usuario (id_usuario, id_linha, nome, data_nascimento, cpf, email, telefone, bairro)
+VALUES ('415', '184', 'Anderson Demilson', '1967-07-24', '60286581035','Andersondemilson@gmail.com', '15068-1275' ,'Carvoeira');
